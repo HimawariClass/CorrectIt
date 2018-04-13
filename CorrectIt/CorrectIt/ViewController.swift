1d70d5a7ea1da287e82fe7cf381ffc0d62000b62
Taillook <jousoku@gmail.com>
2018-04-13 14:53:19 +0900

    [ADD] project

---
+++ CorrectIt/CorrectIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CorrectIt
+//
+//  Created by Taillook on 2018/04/13.
+//  Copyright © 2018年 HimawariClass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
