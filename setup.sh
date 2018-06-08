ad5208737dd4bf62eb6d0b02fc737c8de5742f50
Taillook <jousoku@gmail.com>
2018-06-08 13:37:48 +0900

    [UPDATE] opencv2 import script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-wget -O CorrectIt/CorrectIt/ https://github.com/opencv/opencv/releases/download/3.4.1/opencv-3.4.1-ios-framework.zip
-unzip -d CorrectIt/CorrectIt/ -o opencv-3.4.1-ios-framework.zip
+wget -O CorrectIt/CorrectIt/opencv-3.4.1-ios-framework.zip https://github.com/opencv/opencv/releases/download/3.4.1/opencv-3.4.1-ios-framework.zip
+unzip -d CorrectIt/CorrectIt/ -o CorrectIt/CorrectIt/opencv-3.4.1-ios-framework.zip
