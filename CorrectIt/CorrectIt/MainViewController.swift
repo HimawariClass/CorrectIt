76ca79107be49774f3a598bdc6f265201f9f11e5
Taillook <jousoku@gmail.com>
2018-06-09 11:00:02 +0900

    [UPDATE] inital page

--- CorrectIt/CorrectIt/MainViewController.swift
+++ CorrectIt/CorrectIt/MainViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
+        self.title = "CorrectIt"
+        self.view.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
