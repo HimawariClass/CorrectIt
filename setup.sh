a09a7962b1a602cb8acce42cc5a76d6e24e7efab
Taillook <jousoku@gmail.com>
2018-06-22 14:00:37 +0900

    delete setup.sh

--- setup.sh
+++
@@ -1,2 +0,0 @@
-wget -O CorrectIt/CorrectIt/opencv-3.4.1-ios-framework.zip https://github.com/opencv/opencv/releases/download/3.4.1/opencv-3.4.1-ios-framework.zip
-unzip -d CorrectIt/CorrectIt/ -o CorrectIt/CorrectIt/opencv-3.4.1-ios-framework.zip
