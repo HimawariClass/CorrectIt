45e2a19009e1e3d459f9b5b4c298ee69a39acdc2
Taillook <jousoku@gmail.com>
2019-01-22 13:29:17 +0900

    [ADD] getRGBFromHEX

--- CorrectIt/CorrectIt/ColorOperation.swift
+++ CorrectIt/CorrectIt/ColorOperation.swift
@@ -23,4 +23,14 @@ class ColorOperation {
             return false
         }
     }
+    
+    func getRGBFromHEX(hex: String) -> [Int] {
+        var hexStr = hex
+        hexStr.replaceSubrange(hexStr.range(of: "#")!, with: "")
+        let v = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+        let r = Int(Float(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0)
+        let g = Int(Float(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0)
+        let b = Int(Float(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0)
+        return [r,g,b]
+    }
 }
